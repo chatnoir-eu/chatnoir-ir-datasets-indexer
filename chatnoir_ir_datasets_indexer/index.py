e1b735fbd97b41794ac5135d0c043e02611ab4ad
Maik Fröbe <maik.froebe@uni-jena.de>
2024-10-31 17:28:56 +0100

    add msmarco v1

--- chatnoir_ir_datasets_indexer/index.py
+++ chatnoir_ir_datasets_indexer/index.py
@@ -347,10 +347,10 @@ def _dataset_mapping(dataset_id: str) -> DatasetMapping:
     if dataset_id.startswith("clueweb22"):
         from chatnoir_ir_datasets_indexer.clueweb22_mapping import ClueWeb22Mapping
         return ClueWeb22Mapping()
-    if dataset_id.startswith("msmarco-document-v1"):
+    if dataset_id.startswith("msmarco-document"):
         from chatnoir_ir_datasets_indexer.msmarcov1_mapping import MsMarcoV1DocumentMapping
         return MsMarcoV1DocumentMapping()
-    if dataset_id.startswith("msmarco-passage-v1"):
+    if dataset_id.startswith("msmarco-passage"):
         from chatnoir_ir_datasets_indexer.msmarcov1_mapping import MsMarcoV1PassageMapping
         return MsMarcoV1PassageMapping()
     if dataset_id.startswith("msmarco-document-v2.1/segmented"):
